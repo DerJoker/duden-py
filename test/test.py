efa31994540607ea622d6a4d4e4781dd1ef6ec89
Ya Feng <fengya09@gmail.com>
2015-01-30 23:13:39 +0800

    s.replace().replace()
    several replaces

--- test/test.py
+++ test/test.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python
 
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(open('test.html'))
-
-print 'item.append() div class="content2"'
-for item in soup.find_all('div', class_ = 'back'):
-    item.append(BeautifulSoup('<div class="content2">something</div>'))
-
-print 'try to find div class="content2"'
-if soup.find_all('div', class_ = 'content2') == []:
-    print 'can\'t find what\'s appended'
-else: print 'find it'
+s = 'abcdefg'
+print s.replace('a','1').replace('c', '3')
