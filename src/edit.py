b287957620f468c315d9cc973c430e62f06e67e7
Ya Feng <fengya09@gmail.com>
2015-02-08 23:10:53 +0800

    v0.1.12
    It’s possible now to add note in anki.html, as following:
    
    <div class="note">er geht zu wenig unter Menschen (schließt sich zu
    sehr ab)</div>

--- src/edit.py
+++ src/edit.py
@@ -1,52 +1,53 @@
-#!/usr/bin/python
+# coding: UTF-8
 
 from bs4 import BeautifulSoup
 
 import Dict
-
-def makeHyperlink(display, link):
-    return '<div><a href="' + link + '">' + display + '</a></div>\n'
 
 localDict = Dict.Local()
 
 soup = BeautifulSoup(open('anki.html'))
 
 for item in soup.find_all('div', class_ = 'back'):
-#     try:
-    words = item.string.split()
-    item.string = ''
     s = ''
-    if len(words) == 0:
+    s_ref = '<div class="reflink">'
+    wordList = []
+    for content in item.contents:
+        try:
+            words = content.strip().split('\n')
+            if words != ['']:
+                wordList += words
+                for word in words:
+                    print word
+                    s = s + '\n<div class="note">\n' + word + '\n</div>\n'
+                    # lookup in local duden
+                    if localDict.duden.has_key(word.encode('UTF-8')):
+                        res = localDict.duden[word.encode('UTF-8')]
+                        s = s + '<div class="duden" class="content">'
+                        for definition in res:
+                            s = s + definition['content']
+                            s_ref = s_ref + '<div><a href="' + definition['url'] + '">' + definition['display'] + '</a></div>'
+                        s = s + '</div>'
+                    else: print 'Not found in local Dict Duden:', word
+                    
+                    # lookup in local godic
+                    if localDict.godic.has_key(word.encode('UTF-8')):
+                        res = localDict.godic[word.encode('UTF-8')]
+                        s = s + '<div class="godic" class="content">'
+                        for definition in res:
+                            s = s + definition['content'].decode('utf-8')
+                        s = s + '</div>'
+                    else: print 'Not found in local Dict Godic:', word
+        except:
+            s = s + '\n' + str(content).decode('UTF-8') + '\n'
+    
+    if len(wordList) == 0 and s == '':
         s = '\n<div class="note">\n\n</div>\n'
-    else:
-        s_ref = '<div class="reflink">'
-        for word in words:
-            print word
-            s = s + '\n<div class="note">\n' + word + '\n</div>\n'
-            # lookup in local duden
-            if localDict.duden.has_key(word.encode('UTF-8')):
-                res = localDict.duden[word.encode('UTF-8')]
-                s = s + '<div class="duden" class="content">'
-                for definition in res:
-                    s = s + definition['content']
-                    s_ref = s_ref + '<div><a href="' + definition['url'] + '">' + definition['display'] + '</a></div>'
-                s = s + '</div>'
-            else: print 'Not found in local Dict Duden:', word
-            
-            # lookup in local godic
-            if localDict.godic.has_key(word.encode('UTF-8')):
-                res = localDict.godic[word.encode('UTF-8')]
-                s = s + '<div class="godic" class="content">'
-                for definition in res:
-                    s = s + definition['content'].decode('utf-8')
-                s = s + '</div>'
-            else: print 'Not found in local Dict Godic:', word
-
-        s_ref = s_ref + '</div>'
-        s = s + s_ref
+    
+    s_ref = s_ref + '</div>'
+    s = s + s_ref
+    item.clear()
     item.append(BeautifulSoup(s))
-#     except:
-#         print 'Error in anki.html!'
 
 localDict.save()
 
