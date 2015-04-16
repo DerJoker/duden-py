492aaa2380f3ababdf8616407c9e3e5ee4fa55c3
Ya Feng <fengya09@gmail.com>
2015-04-16 22:07:43 +0800

    done.txt and rechtschreibung.txt

--- test/DudenTest.py
+++ test/DudenTest.py
@@ -9,34 +9,51 @@ from anki import CardExample
 
 done = 'done.txt'
 pool = 'pool.txt'
+rs = 'rechtschreibung.txt'
 
 def readList(txt):
-    return eval(open(txt))
+    try:
+        return eval(open(txt,'r').read())
+    except:
+        return []
 
 def writeList(l,txt):
-    f = open(txt)
-    f.write(l)
+    f = open(txt,'w')
+    f.write(str(l))
     f.close()
 
 t_rechtschreibung = 'sicher_gefahrlos_tadellos_garantiert'
 # t_rechtschreibung = 'Fall_stuerzen_hinfallen_Sache'
 # t_rechtschreibung = 'schlimm'
 # t_rechtschreibung = 'Moment_Zeitpunkt_Zeitspanne'
-t_rechtschreibung = 'Richtung'
-t_rechtschreibung = 'blosz_Adjektiv'
+# t_rechtschreibung = 'Richtung'
+# t_rechtschreibung = 'blosz_Adjektiv'
 
-rs = Rechtschreibung(t_rechtschreibung)
+ldone = readList(done)
+print ldone
+tmp = readList(rs)
+print tmp
 
-print rs.wort
+if len(tmp) == 0:
+    tmp.append(t_rechtschreibung)
 
-l = rs.getRechtschreibungOnPage()
- 
-for item in l:
-    print item
-    irs = Rechtschreibung(item)
-     
-    irs.downloadMP3()
-     
-    for card in irs.makeCardExample():
-        print card
-        print '\n'
+lrs = []
+
+for item in tmp:
+    if item not in ldone:
+        ldone.append(item)
+        
+        irs = Rechtschreibung(item)
+        irs.downloadMP3()
+        for card in irs.makeCardExample():
+            print card
+        
+        lrs.extend(irs.getRechtschreibungOnPage())
+
+lrs.extend(tmp)
+
+lrs = list(set(lrs))
+print lrs
+print len(lrs)
+writeList(lrs, rs)
+writeList(ldone, done)
