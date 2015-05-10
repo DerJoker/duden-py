fac194225f9c66a291e938f7fa6c803e73b643e6
Ya Feng <fengya09@gmail.com>
2015-05-10 21:53:38 +0800

    add comments
    1. bugfix: str(l) -> [unicode(item) for item in l]
    2. bug: find definitions based on examples not right (x)
    3. bug: ascii codec can't encode character …

--- src/duden.py
+++ src/duden.py
@@ -83,14 +83,14 @@ class Rechtschreibung:
     Return list of Bedeutungen / definitions with examples, if there is.
     '''
     def sliceBedeutungen(self):
-        l = self.soup.find_all('div',class_='Beispiele')
+        # <span class="content">Bedeutung...<div class="Beispiele">...</div>...</span>
+        l = [item.parent for item in self.soup.find_all('div',class_='Beispiele')]
         
-        if len(l) > 0:
-            l = [item.parent for item in l]
-        else:
+        # if empty, at least we should find Bedeutung / definition
+        if l == []:
             l = self.soup.find_all('div',class_='field-name-field-abstract')
         
-        return str(l)
+        return [unicode(item) for item in l]
     
     # href="/rechtschreibung/weit" -> href="http://www.duden.de/rechtschreibung/weit"
     def completeRechtschreibungLinks(self):
@@ -248,19 +248,19 @@ if __name__ == '__main__':
 #     f_log = open('log.txt','w')
     
     # Rechtschreibung list for test
-#     lt = [u'verhe̲e̲rend', u'Taetigkeit', u'Blickwinkel', u'scheiden', u'Ehe', u'beobachten', u'modern_neu_modisch', u'schmuck', u'drauf', u'Anleitung']
-    
+    lt = [u'Chip', u'verhe̲e̲rend', u'Taetigkeit', u'Blickwinkel', u'scheiden', u'Ehe', u'beobachten', u'modern_neu_modisch', u'schmuck', u'drauf', u'Anleitung']
+    lt = [u'verhe̲e̲rend']
     # WZD in wzd_list.txt
 #     lt = [item.strip() for item in open('wzd_list.txt').readlines()]
     
     # special test cases of Sound
-    lt = [u'Aenderung', u'Aktivitaet', u'Anbieter', u'andere', u'Angestellter', u'ausreichend', u'Bedienungsanleitung', u'begeistert', u'Begruendung', u'Beratung', u'Bestaetigung', u'beste_Adjektiv', u'bestimmt_Adverb', u'Betreuung', u'Deutscher', u'Einfuehrung', u'Erhoehung', u'geboten_bieten', u'gestorben', u'Hersteller', u'interkulturell', u'Kindertagesstaette', u'langweilen', u'linke', u'Manager', u'maximal', u'naechste', u'Schlaf', u'Sekretaer', u'Traum_', u'vorige', u'aktiv', u'Alkohol', u'allerdings_Adverb', u'Artikel', u'Augenblick', u'augenblicklich', u'ausgezeichnet', u'benutzen', u'Cent', u'Chemie', u'dagegen', u'danach', u'darauf', u'darueber', u'deswegen', u'Dusche', u'Fleck', u'Geburt', u'gefallen_fallen', u'Geografie', u'gern', u'Hamburger_Speise_Gericht', u'Interesse', u'interessieren', u'Interview', u'Kilometer', u'Kredit_Finanzierung_Anleihe', u'Kritik', u'kritisch', u'Kurve', u'Langeweile', u'Motor', u'Motorrad', u'nachher', u'nutzen', u'passiv', u'Politik', u'Politiker', u'positiv', u'Scheck_Zahlungsanweisung_Bon', u'selbststaendig', u'Souvenir', u'Star_Kuenstler_Beruehmtheit', u'Stress', u'tatsaechlich_sicher_gewiss_garantiert', u'ueberall', u'unbedingt_zwingend', u'unheimlich', u'vorwaerts', u'Zentimeter', u'Arbeitnehmer', u'Balkon', u'Chance', u'dadurch', u'daher', u'darum', u'Mathematik', u'negativ', u'Saison', u'alltaeglich', u'Pension', u'Abteilung']
+#     lt = [u'Aenderung', u'Aktivitaet', u'Anbieter', u'andere', u'Angestellter', u'ausreichend', u'Bedienungsanleitung', u'begeistert', u'Begruendung', u'Beratung', u'Bestaetigung', u'beste_Adjektiv', u'bestimmt_Adverb', u'Betreuung', u'Deutscher', u'Einfuehrung', u'Erhoehung', u'geboten_bieten', u'gestorben', u'Hersteller', u'interkulturell', u'Kindertagesstaette', u'langweilen', u'linke', u'Manager', u'maximal', u'naechste', u'Schlaf', u'Sekretaer', u'Traum_', u'vorige', u'aktiv', u'Alkohol', u'allerdings_Adverb', u'Artikel', u'Augenblick', u'augenblicklich', u'ausgezeichnet', u'benutzen', u'Cent', u'Chemie', u'dagegen', u'danach', u'darauf', u'darueber', u'deswegen', u'Dusche', u'Fleck', u'Geburt', u'gefallen_fallen', u'Geografie', u'gern', u'Hamburger_Speise_Gericht', u'Interesse', u'interessieren', u'Interview', u'Kilometer', u'Kredit_Finanzierung_Anleihe', u'Kritik', u'kritisch', u'Kurve', u'Langeweile', u'Motor', u'Motorrad', u'nachher', u'nutzen', u'passiv', u'Politik', u'Politiker', u'positiv', u'Scheck_Zahlungsanweisung_Bon', u'selbststaendig', u'Souvenir', u'Star_Kuenstler_Beruehmtheit', u'Stress', u'tatsaechlich_sicher_gewiss_garantiert', u'ueberall', u'unbedingt_zwingend', u'unheimlich', u'vorwaerts', u'Zentimeter', u'Arbeitnehmer', u'Balkon', u'Chance', u'dadurch', u'daher', u'darum', u'Mathematik', u'negativ', u'Saison', u'alltaeglich', u'Pension', u'Abteilung']
     
     for item in lt:
-        print item
         rs = Rechtschreibung(item)
         
 #         print rs.sliceAussprache()
+        print item, ':', len(rs.sliceBedeutungen())
         print rs.sliceBedeutungen()
         
 #         analyser = Analyser(rs.html)
