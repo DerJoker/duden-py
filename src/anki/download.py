3850021e060651ffdebeffa688f21004a95c2079
Ya Feng <fengya09@gmail.com>
2015-07-21 22:24:45 +0800

    bugfix
    error if folder media not exists.

--- src/anki/download.py
+++ src/anki/download.py
@@ -4,11 +4,16 @@ import os.path
 
 import tools
 
+fdn_media = 'media'
+
+if not os.path.exists(fdn_media):
+    os.makedirs(fdn_media)
+
 f_anki_links = open('links.txt')
 
 for link in f_anki_links.readlines():
     link = link.strip()
-    local = os.path.join('media', link.split('/')[-1])
+    local = os.path.join(fdn_media, link.split('/')[-1])
     if not os.path.exists(local):
         tools.download(link, local)
 
